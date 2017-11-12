ed4dedbb510a335089a895e16c47ed86d9fc0733
Matthis Duclos <matthis.duclos@gmail.com>
2017-11-12 21:17:11 +0100

    0.1.2

--- dist/react-hoc-promises.cjs.js
+++ dist/react-hoc-promises.cjs.js
@@ -1,10 +1,12 @@
 'use strict';
 
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+function _interopDefault(ex) {
+  return ex && typeof ex === 'object' && 'default' in ex ? ex['default'] : ex;
+}
 
 var React = _interopDefault(require('react'));
 
-var asyncGenerator = function () {
+var asyncGenerator = (function() {
   function AwaitValue(value) {
     this.value = value;
   }
@@ -13,13 +15,13 @@ var asyncGenerator = function () {
     var front, back;
 
     function send(key, arg) {
-      return new Promise(function (resolve, reject) {
+      return new Promise(function(resolve, reject) {
         var request = {
           key: key,
           arg: arg,
           resolve: resolve,
           reject: reject,
-          next: null
+          next: null,
         };
 
         if (back) {
@@ -37,36 +39,39 @@ var asyncGenerator = function () {
         var value = result.value;
 
         if (value instanceof AwaitValue) {
-          Promise.resolve(value.value).then(function (arg) {
-            resume("next", arg);
-          }, function (arg) {
-            resume("throw", arg);
-          });
+          Promise.resolve(value.value).then(
+            function(arg) {
+              resume('next', arg);
+            },
+            function(arg) {
+              resume('throw', arg);
+            },
+          );
         } else {
-          settle(result.done ? "return" : "normal", result.value);
+          settle(result.done ? 'return' : 'normal', result.value);
         }
       } catch (err) {
-        settle("throw", err);
+        settle('throw', err);
       }
     }
 
     function settle(type, value) {
       switch (type) {
-        case "return":
+        case 'return':
           front.resolve({
             value: value,
-            done: true
+            done: true,
           });
           break;
 
-        case "throw":
+        case 'throw':
           front.reject(value);
           break;
 
         default:
           front.resolve({
             value: value,
-            done: false
+            done: false,
           });
           break;
       }
@@ -82,80 +87,72 @@ var asyncGenerator = function () {
 
     this._invoke = send;
 
-    if (typeof gen.return !== "function") {
+    if (typeof gen.return !== 'function') {
       this.return = undefined;
     }
   }
 
-  if (typeof Symbol === "function" && Symbol.asyncIterator) {
-    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+  if (typeof Symbol === 'function' && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function() {
       return this;
     };
   }
 
-  AsyncGenerator.prototype.next = function (arg) {
-    return this._invoke("next", arg);
+  AsyncGenerator.prototype.next = function(arg) {
+    return this._invoke('next', arg);
   };
 
-  AsyncGenerator.prototype.throw = function (arg) {
-    return this._invoke("throw", arg);
+  AsyncGenerator.prototype.throw = function(arg) {
+    return this._invoke('throw', arg);
   };
 
-  AsyncGenerator.prototype.return = function (arg) {
-    return this._invoke("return", arg);
+  AsyncGenerator.prototype.return = function(arg) {
+    return this._invoke('return', arg);
   };
 
   return {
-    wrap: function (fn) {
-      return function () {
+    wrap: function(fn) {
+      return function() {
         return new AsyncGenerator(fn.apply(this, arguments));
       };
     },
-    await: function (value) {
+    await: function(value) {
       return new AwaitValue(value);
-    }
+    },
   };
-}();
+})();
 
-
-
-
-
-var classCallCheck = function (instance, Constructor) {
+var classCallCheck = function(instance, Constructor) {
   if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
+    throw new TypeError('Cannot call a class as a function');
   }
 };
 
-var createClass = function () {
+var createClass = (function() {
   function defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
       var descriptor = props[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
+      if ('value' in descriptor) descriptor.writable = true;
       Object.defineProperty(target, descriptor.key, descriptor);
     }
   }
 
-  return function (Constructor, protoProps, staticProps) {
+  return function(Constructor, protoProps, staticProps) {
     if (protoProps) defineProperties(Constructor.prototype, protoProps);
     if (staticProps) defineProperties(Constructor, staticProps);
     return Constructor;
   };
-}();
+})();
 
-
-
-
-
-var defineProperty = function (obj, key, value) {
+var defineProperty = function(obj, key, value) {
   if (key in obj) {
     Object.defineProperty(obj, key, {
       value: value,
       enumerable: true,
       configurable: true,
-      writable: true
+      writable: true,
     });
   } else {
     obj[key] = value;
@@ -164,25 +161,28 @@ var defineProperty = function (obj, key, value) {
   return obj;
 };
 
-var _extends = Object.assign || function (target) {
-  for (var i = 1; i < arguments.length; i++) {
-    var source = arguments[i];
+var _extends =
+  Object.assign ||
+  function(target) {
+    for (var i = 1; i < arguments.length; i++) {
+      var source = arguments[i];
 
-    for (var key in source) {
-      if (Object.prototype.hasOwnProperty.call(source, key)) {
-        target[key] = source[key];
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
       }
     }
-  }
 
-  return target;
-};
+    return target;
+  };
 
-
-
-var inherits = function (subClass, superClass) {
-  if (typeof superClass !== "function" && superClass !== null) {
-    throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);
+var inherits = function(subClass, superClass) {
+  if (typeof superClass !== 'function' && superClass !== null) {
+    throw new TypeError(
+      'Super expression must either be null or a function, not ' +
+        typeof superClass,
+    );
   }
 
   subClass.prototype = Object.create(superClass && superClass.prototype, {
@@ -190,76 +190,106 @@ var inherits = function (subClass, superClass) {
       value: subClass,
       enumerable: false,
       writable: true,
-      configurable: true
-    }
+      configurable: true,
+    },
   });
-  if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;
+  if (superClass)
+    Object.setPrototypeOf
+      ? Object.setPrototypeOf(subClass, superClass)
+      : (subClass.__proto__ = superClass);
 };
 
-
-
-
-
-
-
-
-
-
-
-var possibleConstructorReturn = function (self, call) {
+var possibleConstructorReturn = function(self, call) {
   if (!self) {
-    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    throw new ReferenceError(
+      "this hasn't been initialised - super() hasn't been called",
+    );
   }
 
-  return call && (typeof call === "object" || typeof call === "function") ? call : self;
+  return call && (typeof call === 'object' || typeof call === 'function')
+    ? call
+    : self;
 };
 
+function getDisplayName(WrappedComponent) {
+  return WrappedComponent.displayName || WrappedComponent.name || 'Component';
+}
+
 var withPromises = function withPromises(mapPromisesToProps) {
-  return function (WrappedComponent) {
-    var WithPromises = function (_React$Component) {
+  return function(WrappedComponent) {
+    var WithPromises = (function(_React$Component) {
       inherits(WithPromises, _React$Component);
 
       function WithPromises(props) {
         classCallCheck(this, WithPromises);
 
-        var _this = possibleConstructorReturn(this, (WithPromises.__proto__ || Object.getPrototypeOf(WithPromises)).call(this, props));
+        var _this = possibleConstructorReturn(
+          this,
+          (WithPromises.__proto__ || Object.getPrototypeOf(WithPromises)).call(
+            this,
+            props,
+          ),
+        );
 
         _this.state = { loading: true };
         return _this;
       }
 
-      createClass(WithPromises, [{
-        key: 'componentDidMount',
-        value: function componentDidMount() {
-          var _this2 = this;
+      createClass(WithPromises, [
+        {
+          key: 'componentDidMount',
+          value: function componentDidMount() {
+            var _this2 = this;
 
-          return Promise.all(Object.values(mapPromisesToProps)).then(function (datas) {
-            var state = Object.keys(mapPromisesToProps).reduce(function (acc, key, index) {
-              return _extends({}, acc, defineProperty({}, key, datas[index]));
-            }, {});
-            _this2.setState(_extends({
-              loading: false,
-              error: null
-            }, state));
-            return datas;
-          }).catch(function (err) {
-            _this2.setState({
-              loading: false,
-              error: err
-            });
-            return err;
-          });
-        }
-      }, {
-        key: 'render',
-        value: function render() {
-          return React.createElement(WrappedComponent, _extends({}, this.props, this.state));
-        }
-      }]);
+            return Promise.all(Object.values(mapPromisesToProps))
+              .then(function(datas) {
+                var state = Object.keys(mapPromisesToProps).reduce(function(
+                  acc,
+                  key,
+                  index,
+                ) {
+                  return _extends(
+                    {},
+                    acc,
+                    defineProperty({}, key, datas[index]),
+                  );
+                },
+                {});
+                _this2.setState(
+                  _extends(
+                    {
+                      loading: false,
+                      error: null,
+                    },
+                    state,
+                  ),
+                );
+                return datas;
+              })
+              .catch(function(err) {
+                _this2.setState({
+                  loading: false,
+                  error: err,
+                });
+                return err;
+              });
+          },
+        },
+        {
+          key: 'render',
+          value: function render() {
+            return React.createElement(
+              WrappedComponent,
+              _extends({}, this.props, this.state),
+            );
+          },
+        },
+      ]);
       return WithPromises;
-    }(React.Component);
+    })(React.Component);
 
-    WithPromises.displayName = 'withPromises(' + WrappedComponent.displayName + ')';
+    WithPromises.displayName =
+      'withPromises(' + getDisplayName(WrappedComponent) + ')';
 
     return WithPromises;
   };
